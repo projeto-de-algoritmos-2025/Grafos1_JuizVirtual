1855471b5f00e35318e5dacdd49aefb6cd879c0f
Thales Euflauzino <thaleseuflauzino@gmail.com>
2025-04-12 20:06:02 -0300

    Adição de nós vizinhos à fila de prioridade com controle de movimentos restantes

--- 882.py
+++ 882.py
@@ -13,11 +13,20 @@ class Solution:
         # Dijkstra com heap de min caminho a partir do no 0
         heap = [(-maxMoves, 0)] 
         visited = {}
-        
+
         while heap:
             moves_left, node = heapq.heappop(heap)
-            moves_left = -moves_left  # Inverter o sinal para trabalhar com números positivos
+            moves_left = -moves_left  # fazer modulo para ter n positivo
 
             if node in visited:
                 continue
             visited[node] = moves_left
+            # ve os vizinhos do no atual
+            for nei in graph[node]:
+                cnt = graph[node][nei]
+                # verifica se tem vizinho pra add na fila
+                if nei not in visited and moves_left > cnt:
+                    heapq.heappush(heap, (-(moves_left - cnt - 1), nei))
+        # contador dos nos alcancaveis 
+        result = len(visited)
+
