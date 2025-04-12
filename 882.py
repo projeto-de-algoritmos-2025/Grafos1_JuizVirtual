43e097aeb0a4bf5425d52d687af0c9338c692d3b
Thales Euflauzino <thaleseuflauzino@gmail.com>
2025-04-12 19:32:54 -0300

    feat: retirando biblioteca nao utilizada

--- 882.py
+++ 882.py
@@ -1,10 +1,8 @@
 from typing import List
-import heapq
-from collections import defaultdict
+from collections import deque, defaultdict
 
 class Solution:
     def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
-        # Criar grafo como um dicionário de adjacência
         graph = defaultdict(dict)
         for u, v, cnt in edges:
             graph[u][v] = cnt
