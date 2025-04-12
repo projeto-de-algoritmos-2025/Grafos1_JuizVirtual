89939087535fdee941eac9502a9b95433312c4bf
Thales Euflauzino <thaleseuflauzino@gmail.com>
2025-04-12 20:00:36 -0300

    Implementação do algoritmo de Dijkstra com heap para calcular os menores caminhos

--- 882.py
+++ 882.py
@@ -1,9 +1,15 @@
 from typing import List
-from collections import deque, defaultdict
+import heapq
+from collections import defaultdict
 
 class Solution:
     def reachableNodes(self, edges: List[List[int]], maxMoves: int, n: int) -> int:
+        # Criar grafo como um dicionário de adjacência
         graph = defaultdict(dict)
         for u, v, cnt in edges:
             graph[u][v] = cnt
             graph[v][u] = cnt
+
+        # Dijkstra com heap de min caminho a partir do no 0
+        heap = [(-maxMoves, 0)] 
+        visited = {}
